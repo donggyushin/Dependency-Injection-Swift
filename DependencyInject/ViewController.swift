601c08690f8615114b5c443c9459b004ccbf74b8
shin donggyu <donggyu9410@gmail.com>
2021-01-06 20:18:55 +0900

    프로젝트 생성 :)

---
+++ DependencyInject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DependencyInject
+//
+//  Created by 신동규 on 2021/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
